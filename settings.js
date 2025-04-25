184c8268f1338283a39da53e8560cdd54040631c
saqibimran333 <saqibimrantsi@gmail.com>
2025-04-25 12:23:16 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'TalkNest',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "TalkNest",
+  botOwner: 'Saqib Imran', // TalkNest
+  ownerNumber: '923418501562', //923256208662
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
